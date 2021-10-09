e19db2a994538bf3b3743f1392dca189fa3ba93f
NSJ-backup <ghdlghdl0518@hanmail.net>
2021-10-10 01:04:41 +0900

    bayer2rgb 오픈소스를 가져와 dll을 생성하여 C# UI에 사용

--- RawImageViewer/Form1.Designer.cs
+++ RawImageViewer/Form1.Designer.cs
@@ -29,28 +29,31 @@
         private void InitializeComponent()
         {
             this.spltContainer = new System.Windows.Forms.SplitContainer();
-            this.pResult = new System.Windows.Forms.Panel();
             this.tlpControl = new System.Windows.Forms.TableLayoutPanel();
-            this.btnPixelSet = new System.Windows.Forms.Button();
-            this.btnOpen = new System.Windows.Forms.Button();
-            this.lblPixelSet = new System.Windows.Forms.Label();
+            this.lblSize = new System.Windows.Forms.Label();
             this.lblFileOpen = new System.Windows.Forms.Label();
             this.tbImagePath = new System.Windows.Forms.TextBox();
-            this.lblSize = new System.Windows.Forms.Label();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.lblPixelSet = new System.Windows.Forms.Label();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.tbWidth = new System.Windows.Forms.TextBox();
             this.lblX = new System.Windows.Forms.Label();
             this.tbHeight = new System.Windows.Forms.TextBox();
             this.lblPixelSize = new System.Windows.Forms.Label();
             this.tbPixelSize = new System.Windows.Forms.TextBox();
-            this.flpPixelButton = new System.Windows.Forms.FlowLayoutPanel();
             this.progressRawImage = new System.Windows.Forms.ProgressBar();
+            this.flpPixelButton = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnPixelSet = new System.Windows.Forms.Button();
+            this.pResult = new System.Windows.Forms.Panel();
+            this.pictureResult = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.spltContainer)).BeginInit();
             this.spltContainer.Panel1.SuspendLayout();
             this.spltContainer.Panel2.SuspendLayout();
             this.spltContainer.SuspendLayout();
             this.tlpControl.SuspendLayout();
             this.flowLayoutPanel1.SuspendLayout();
+            this.pResult.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureResult)).BeginInit();
             this.SuspendLayout();
             // 
             // spltContainer
@@ -70,18 +73,8 @@
             this.spltContainer.Panel2.Padding = new System.Windows.Forms.Padding(3);
             this.spltContainer.Panel2MinSize = 100;
             this.spltContainer.Size = new System.Drawing.Size(800, 466);
-            this.spltContainer.SplitterDistance = 361;
+            this.spltContainer.SplitterDistance = 360;
             this.spltContainer.TabIndex = 1;
-            // 
-            // pResult
-            // 
-            this.pResult.AutoScroll = true;
-            this.pResult.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pResult.Location = new System.Drawing.Point(0, 0);
-            this.pResult.Name = "pResult";
-            this.pResult.Size = new System.Drawing.Size(800, 361);
-            this.pResult.TabIndex = 0;
-            this.pResult.Paint += new System.Windows.Forms.PaintEventHandler(this.pResult_Paint);
             // 
             // tlpControl
             // 
@@ -105,24 +98,42 @@
             this.tlpControl.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.tlpControl.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.tlpControl.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tlpControl.Size = new System.Drawing.Size(794, 95);
+            this.tlpControl.Size = new System.Drawing.Size(794, 96);
             this.tlpControl.TabIndex = 0;
             // 
-            // btnPixelSet
+            // lblSize
             // 
-            this.btnPixelSet.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnPixelSet.Location = new System.Drawing.Point(697, 4);
-            this.btnPixelSet.Name = "btnPixelSet";
-            this.btnPixelSet.Size = new System.Drawing.Size(94, 23);
-            this.btnPixelSet.TabIndex = 0;
-            this.btnPixelSet.Text = "Set";
-            this.btnPixelSet.UseVisualStyleBackColor = true;
-            this.btnPixelSet.Click += new System.EventHandler(this.btnPixelSet_Click);
+            this.lblSize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblSize.AutoSize = true;
+            this.lblSize.Location = new System.Drawing.Point(3, 10);
+            this.lblSize.Name = "lblSize";
+            this.lblSize.Size = new System.Drawing.Size(94, 12);
+            this.lblSize.TabIndex = 5;
+            this.lblSize.Text = "Size";
+            // 
+            // lblFileOpen
+            // 
+            this.lblFileOpen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblFileOpen.AutoSize = true;
+            this.lblFileOpen.Location = new System.Drawing.Point(3, 74);
+            this.lblFileOpen.Name = "lblFileOpen";
+            this.lblFileOpen.Size = new System.Drawing.Size(94, 12);
+            this.lblFileOpen.TabIndex = 3;
+            this.lblFileOpen.Text = "Raw File";
+            // 
+            // tbImagePath
+            // 
+            this.tbImagePath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbImagePath.Location = new System.Drawing.Point(103, 69);
+            this.tbImagePath.Name = "tbImagePath";
+            this.tbImagePath.ReadOnly = true;
+            this.tbImagePath.Size = new System.Drawing.Size(588, 21);
+            this.tbImagePath.TabIndex = 4;
             // 
             // btnOpen
             // 
             this.btnOpen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnOpen.Location = new System.Drawing.Point(697, 67);
+            this.btnOpen.Location = new System.Drawing.Point(697, 68);
             this.btnOpen.Name = "btnOpen";
             this.btnOpen.Size = new System.Drawing.Size(94, 23);
             this.btnOpen.TabIndex = 1;
@@ -134,40 +145,11 @@
             // 
             this.lblPixelSet.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.lblPixelSet.AutoSize = true;
-            this.lblPixelSet.Location = new System.Drawing.Point(3, 40);
+            this.lblPixelSet.Location = new System.Drawing.Point(3, 42);
             this.lblPixelSet.Name = "lblPixelSet";
             this.lblPixelSet.Size = new System.Drawing.Size(94, 12);
             this.lblPixelSet.TabIndex = 2;
             this.lblPixelSet.Text = "Pixel Setting";
-            // 
-            // lblFileOpen
-            // 
-            this.lblFileOpen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.lblFileOpen.AutoSize = true;
-            this.lblFileOpen.Location = new System.Drawing.Point(3, 72);
-            this.lblFileOpen.Name = "lblFileOpen";
-            this.lblFileOpen.Size = new System.Drawing.Size(94, 12);
-            this.lblFileOpen.TabIndex = 3;
-            this.lblFileOpen.Text = "Raw File";
-            // 
-            // tbImagePath
-            // 
-            this.tbImagePath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.tbImagePath.Location = new System.Drawing.Point(103, 68);
-            this.tbImagePath.Name = "tbImagePath";
-            this.tbImagePath.ReadOnly = true;
-            this.tbImagePath.Size = new System.Drawing.Size(588, 21);
-            this.tbImagePath.TabIndex = 4;
-            // 
-            // lblSize
-            // 
-            this.lblSize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.lblSize.AutoSize = true;
-            this.lblSize.Location = new System.Drawing.Point(3, 9);
-            this.lblSize.Name = "lblSize";
-            this.lblSize.Size = new System.Drawing.Size(94, 12);
-            this.lblSize.TabIndex = 5;
-            this.lblSize.Text = "Size";
             // 
             // flowLayoutPanel1
             // 
@@ -180,7 +162,7 @@
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.flowLayoutPanel1.Location = new System.Drawing.Point(103, 3);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(588, 25);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(588, 26);
             this.flowLayoutPanel1.TabIndex = 6;
             // 
             // tbWidth
@@ -236,15 +218,6 @@
             this.tbPixelSize.Text = "16";
             this.tbPixelSize.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbPixelSize_KeyPress);
             // 
-            // flpPixelButton
-            // 
-            this.tlpControl.SetColumnSpan(this.flpPixelButton, 2);
-            this.flpPixelButton.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.flpPixelButton.Location = new System.Drawing.Point(103, 34);
-            this.flpPixelButton.Name = "flpPixelButton";
-            this.flpPixelButton.Size = new System.Drawing.Size(688, 25);
-            this.flpPixelButton.TabIndex = 7;
-            // 
             // progressRawImage
             // 
             this.progressRawImage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
@@ -254,12 +227,56 @@
             this.progressRawImage.Size = new System.Drawing.Size(243, 21);
             this.progressRawImage.TabIndex = 5;
             // 
+            // flpPixelButton
+            // 
+            this.tlpControl.SetColumnSpan(this.flpPixelButton, 2);
+            this.flpPixelButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flpPixelButton.Location = new System.Drawing.Point(103, 35);
+            this.flpPixelButton.Name = "flpPixelButton";
+            this.flpPixelButton.Size = new System.Drawing.Size(688, 26);
+            this.flpPixelButton.TabIndex = 7;
+            // 
+            // btnPixelSet
+            // 
+            this.btnPixelSet.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPixelSet.Location = new System.Drawing.Point(697, 4);
+            this.btnPixelSet.Name = "btnPixelSet";
+            this.btnPixelSet.Size = new System.Drawing.Size(94, 23);
+            this.btnPixelSet.TabIndex = 0;
+            this.btnPixelSet.Text = "Set";
+            this.btnPixelSet.UseVisualStyleBackColor = true;
+            this.btnPixelSet.Click += new System.EventHandler(this.btnPixelSet_Click);
+            // 
+            // pResult
+            // 
+            this.pResult.AutoScroll = true;
+            this.pResult.Controls.Add(this.pictureResult);
+            this.pResult.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pResult.Location = new System.Drawing.Point(0, 0);
+            this.pResult.Name = "pResult";
+            this.pResult.Size = new System.Drawing.Size(800, 360);
+            this.pResult.TabIndex = 0;
+            this.pResult.Paint += new System.Windows.Forms.PaintEventHandler(this.pResult_Paint);
+            this.pResult.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pResult_MouseDown);
+            this.pResult.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pResult_MouseMove);
+            this.pResult.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pResult_MouseUp);
+            // 
+            // pictureResult
+            // 
+            this.pictureResult.Location = new System.Drawing.Point(0, 0);
+            this.pictureResult.Name = "pictureResult";
+            this.pictureResult.Size = new System.Drawing.Size(100, 50);
+            this.pictureResult.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureResult.TabIndex = 0;
+            this.pictureResult.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 466);
             this.Controls.Add(this.spltContainer);
+            this.DoubleBuffered = true;
             this.Name = "Form1";
             this.Text = "Raw Image Viewer";
             this.spltContainer.Panel1.ResumeLayout(false);
@@ -270,6 +287,9 @@
             this.tlpControl.PerformLayout();
             this.flowLayoutPanel1.ResumeLayout(false);
             this.flowLayoutPanel1.PerformLayout();
+            this.pResult.ResumeLayout(false);
+            this.pResult.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureResult)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -277,7 +297,6 @@
         #endregion
 
         private System.Windows.Forms.SplitContainer spltContainer;
-        private System.Windows.Forms.Panel pResult;
         private System.Windows.Forms.TableLayoutPanel tlpControl;
         private System.Windows.Forms.Button btnPixelSet;
         private System.Windows.Forms.Button btnOpen;
@@ -293,6 +312,8 @@
         private System.Windows.Forms.TextBox tbPixelSize;
         private System.Windows.Forms.FlowLayoutPanel flpPixelButton;
         private System.Windows.Forms.ProgressBar progressRawImage;
+        private System.Windows.Forms.Panel pResult;
+        private System.Windows.Forms.PictureBox pictureResult;
     }
 }
 
